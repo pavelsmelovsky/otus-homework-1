5328eff6994a8b1f2542e0fca56e5b7a93ebacf4
Pavel Smelovsky <pavel.smelovsky@gmail.com>
2019-09-15 13:52:58 +0300

    Initial Commit

---
+++ HomeWork-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork-1
+//
+//  Created by Pavel Smelovsky on 15/09/2019.
+//  Copyright © 2019 Pavel Smelovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
